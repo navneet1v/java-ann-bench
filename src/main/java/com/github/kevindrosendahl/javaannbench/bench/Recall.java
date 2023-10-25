44613e8f99b677c246d9e2f4fb39429070899087
Kevin Rosendahl <kevin.rosendahl@mongodb.com>
2023-10-24 21:18:55 -0700

    update query test progress

--- src/main/java/com/github/kevindrosendahl/javaannbench/bench/Recall.java
+++ src/main/java/com/github/kevindrosendahl/javaannbench/bench/Recall.java
@@ -1,5 +1,6 @@
 package com.github.kevindrosendahl.javaannbench.bench;
 
+import com.github.kevindrosendahl.javaannbench.display.ProgressBar;
 import com.github.kevindrosendahl.javaannbench.index.Index;
 import com.google.common.base.Preconditions;
 import io.github.jbellis.jvector.graph.RandomAccessVectorValues;
@@ -11,6 +12,9 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import oshi.SystemInfo;
 
 public class Recall {
+
+  private static final int WARMUP_ITERATIONS = 2;
+  private static final int TEST_ITERATIONS = 3;
 
   public record Results(
       DescriptiveStatistics recall,
@@ -26,38 +30,54 @@ public class Recall {
       throws IOException {
     var systemInfo = new SystemInfo();
     var process = systemInfo.getOperatingSystem().getCurrentProcess();
-
     var numQueries = queries.size();
+
+    try (var progress = ProgressBar.create("warmup", WARMUP_ITERATIONS * numQueries)) {
+      for (int i = 0; i < WARMUP_ITERATIONS; i++) {
+        for (int j = 0; j < numQueries; j++) {
+          var query = queries.vectorValue(j);
+          index.query(query, k);
+          progress.inc();
+        }
+      }
+    }
+
     var recalls = new DescriptiveStatistics();
     var executionDurations = new DescriptiveStatistics();
     var minorFaults = new DescriptiveStatistics();
     var majorFaults = new DescriptiveStatistics();
 
-    for (int i = 0; i < numQueries; i++) {
-      var query = queries.vectorValue(i);
+    try (var progress = ProgressBar.create("testing", TEST_ITERATIONS * numQueries)) {
+      for (int i = 0; i < TEST_ITERATIONS; i++) {
+        for (int j = 0; j < numQueries; j++) {
+          var query = queries.vectorValue(j);
 
-      Preconditions.checkArgument(process.updateAttributes(), "failed to update process stats");
-      var startMinorFaults = process.getMinorFaults();
-      var startMajorFaults = process.getMajorFaults();
+          Preconditions.checkArgument(process.updateAttributes(), "failed to update process stats");
+          var startMinorFaults = process.getMinorFaults();
+          var startMajorFaults = process.getMajorFaults();
 
-      var start = Instant.now();
-      var results = index.query(query, k);
-      var end = Instant.now();
+          var start = Instant.now();
+          var results = index.query(query, k);
+          var end = Instant.now();
 
-      Preconditions.checkArgument(process.updateAttributes(), "failed to update process stats");
-      var endMinorFaults = process.getMinorFaults();
-      var endMajorFaults = process.getMajorFaults();
+          Preconditions.checkArgument(process.updateAttributes(), "failed to update process stats");
+          var endMinorFaults = process.getMinorFaults();
+          var endMajorFaults = process.getMajorFaults();
 
-      var duration = Duration.between(start, end);
-      executionDurations.addValue(duration.toNanos());
+          var duration = Duration.between(start, end);
+          executionDurations.addValue(duration.toNanos());
 
-      var groundTruth = groundTruths.get(i);
-      var truePositives = groundTruth.stream().limit(k).filter(results::contains).count();
-      var recall = (double) truePositives / k;
-      recalls.addValue(recall);
+          var groundTruth = groundTruths.get(j);
+          var truePositives = groundTruth.stream().limit(k).filter(results::contains).count();
+          var recall = (double) truePositives / k;
+          recalls.addValue(recall);
 
-      minorFaults.addValue(endMinorFaults - startMinorFaults);
-      majorFaults.addValue(endMajorFaults - startMajorFaults);
+          minorFaults.addValue(endMinorFaults - startMinorFaults);
+          majorFaults.addValue(endMajorFaults - startMajorFaults);
+
+          progress.inc();
+        }
+      }
     }
 
     return new Results(recalls, executionDurations, minorFaults, majorFaults);
