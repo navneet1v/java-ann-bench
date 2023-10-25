612f5d6289cabd7c85c85cc5101a1e9e90cc7e82
Kevin Rosendahl <kevin.rosendahl@mongodb.com>
2023-10-25 16:41:00 -0700

    jvector pq

--- src/main/java/com/github/kevindrosendahl/javaannbench/index/Index.java
+++ src/main/java/com/github/kevindrosendahl/javaannbench/index/Index.java
@@ -80,7 +80,7 @@ public interface Index extends AutoCloseable {
         case "lucene" -> LuceneIndex.Querier.create(
             indexesPath.resolve(dataset.name()), parameters);
         case "jvector" -> JVectorIndex.Querier.create(
-            datasetPath, dataset.similarityFunction(), parameters);
+            datasetPath, dataset.similarityFunction(), dataset.dimensions(), parameters);
         default -> throw new RuntimeException("unknown index provider: " + parameters.provider);
       };
     }
--- src/main/java/com/github/kevindrosendahl/javaannbench/index/JVectorIndex.java
+++ src/main/java/com/github/kevindrosendahl/javaannbench/index/JVectorIndex.java
@@ -10,10 +10,17 @@ import io.github.jbellis.jvector.disk.OnDiskGraphIndex;
 import io.github.jbellis.jvector.disk.ReaderSupplier;
 import io.github.jbellis.jvector.disk.SimpleMappedReaderSupplier;
 import io.github.jbellis.jvector.graph.GraphIndex;
+import io.github.jbellis.jvector.graph.GraphIndex.View;
 import io.github.jbellis.jvector.graph.GraphIndexBuilder;
 import io.github.jbellis.jvector.graph.GraphSearcher;
+import io.github.jbellis.jvector.graph.ListRandomAccessVectorValues;
 import io.github.jbellis.jvector.graph.NeighborSimilarity;
+import io.github.jbellis.jvector.graph.NeighborSimilarity.ExactScoreFunction;
+import io.github.jbellis.jvector.graph.NeighborSimilarity.ReRanker;
+import io.github.jbellis.jvector.graph.NeighborSimilarity.ScoreFunction;
 import io.github.jbellis.jvector.graph.RandomAccessVectorValues;
+import io.github.jbellis.jvector.pq.CompressedVectors;
+import io.github.jbellis.jvector.pq.ProductQuantization;
 import io.github.jbellis.jvector.util.Bits;
 import io.github.jbellis.jvector.vector.VectorEncoding;
 import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
@@ -30,15 +37,20 @@ import java.util.Optional;
 import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import oshi.SystemInfo;
 
 public class JVectorIndex {
 
-  private static final String INDEX_FILE = "index.bin";
+  private static final Logger LOGGER = LoggerFactory.getLogger(JVectorIndex.class);
+  private static final String GRAPH_FILE = "graph.bin";
+  private static final String COMPRESSED_VECTOR_FILE_FORMAT = "compressed-vectors-%s.bin";
 
   public record BuildParameters(int M, int beamWidth, float neighborOverflow, float alpha) {}
 
-  public record QueryParameters(int numCandidates) {}
+  public record QueryParameters(int numCandidates, int pqFactor) {}
 
   public static final class Builder implements Index.Builder {
 
@@ -112,23 +124,25 @@ public class JVectorIndex {
       var buildStart = Instant.now();
       try (var progress = ProgressBar.create("building", size)) {
         pool.submit(
-            () -> {
-              IntStream.range(0, size)
-                  .parallel()
-                  .forEach(
-                      i -> {
-                        this.indexBuilder.addGraphNode(i, this.vectors);
-                        progress.inc();
-                      });
-            });
+                () -> {
+                  IntStream.range(0, size)
+                      .parallel()
+                      .forEach(
+                          i -> {
+                            this.indexBuilder.addGraphNode(i, this.vectors);
+                            progress.inc();
+                          });
+                })
+            .join();
       }
 
       this.indexBuilder.cleanup();
       var buildEnd = Instant.now();
 
+      LOGGER.info("finished building index, committing");
       var commitStart = Instant.now();
       try (var output =
-          new DataOutputStream(new FileOutputStream(this.indexPath.resolve(INDEX_FILE).toFile()))) {
+          new DataOutputStream(new FileOutputStream(this.indexPath.resolve(GRAPH_FILE).toFile()))) {
         var graph = this.indexBuilder.getGraph();
         OnDiskGraphIndex.write(graph, vectors, output);
       }
@@ -147,7 +161,7 @@ public class JVectorIndex {
 
     @Override
     public Bytes size() throws IOException {
-      return Bytes.ofBytes(Files.size(this.indexPath));
+      return Bytes.ofBytes(FileUtils.sizeOfDirectory(this.indexPath.toFile()));
     }
 
     @Override
@@ -163,6 +177,7 @@ public class JVectorIndex {
   public static class Querier implements Index.Querier {
     private final ReaderSupplier readerSupplier;
     private final GraphIndex<float[]> graph;
+    private final Optional<CompressedVectors> compressedVectors;
     private final VectorSimilarityFunction similarityFunction;
     private final BuildParameters buildParams;
     private final QueryParameters queryParams;
@@ -170,18 +185,23 @@ public class JVectorIndex {
     public Querier(
         ReaderSupplier readerSupplier,
         GraphIndex<float[]> graph,
+        Optional<CompressedVectors> compressedVectors,
         VectorSimilarityFunction similarityFunction,
         BuildParameters buildParams,
         QueryParameters queryParams) {
       this.readerSupplier = readerSupplier;
       this.graph = graph;
+      this.compressedVectors = compressedVectors;
       this.similarityFunction = similarityFunction;
       this.buildParams = buildParams;
       this.queryParams = queryParams;
     }
 
     public static Index.Querier create(
-        Path indexesPath, SimilarityFunction similarityFunction, Parameters parameters)
+        Path indexesPath,
+        SimilarityFunction similarityFunction,
+        int dimensions,
+        Parameters parameters)
         throws IOException {
       Preconditions.checkArgument(
           parameters.type().equals("vamana"),
@@ -194,7 +214,8 @@ public class JVectorIndex {
           Records.fromMap(parameters.queryParameters(), QueryParameters.class, "query parameters");
 
       var buildDescription = JVectorIndex.Builder.buildDescription(buildParams);
-      var path = indexesPath.resolve(buildDescription).resolve(INDEX_FILE);
+      var indexPath = indexesPath.resolve(buildDescription);
+      var path = indexPath.resolve(GRAPH_FILE);
       Preconditions.checkArgument(path.toFile().exists(), "index does not exist at {}", path);
 
       var vectorSimilarityFunction =
@@ -207,9 +228,17 @@ public class JVectorIndex {
       var readerSupplier = new SimpleMappedReaderSupplier(path);
       var onDiskGraph = new OnDiskGraphIndex<float[]>(readerSupplier, 0);
       var cachingGraph = new CachingGraphIndex(onDiskGraph);
+      var compressedVectors =
+          compressedVectors(
+              indexPath, dimensions, queryParams.pqFactor, cachingGraph, vectorSimilarityFunction);
 
       return new JVectorIndex.Querier(
-          readerSupplier, cachingGraph, vectorSimilarityFunction, buildParams, queryParams);
+          readerSupplier,
+          cachingGraph,
+          compressedVectors,
+          vectorSimilarityFunction,
+          buildParams,
+          queryParams);
     }
 
     @Override
@@ -220,7 +249,13 @@ public class JVectorIndex {
           i -> this.similarityFunction.compare(vector, view.getVector(i));
 
       var searcher = new GraphSearcher.Builder<>(view).build();
-      var results = searcher.search(scoreFunction, null, queryParams.numCandidates, Bits.ALL);
+      var querySimilarity = querySimilarity(vector, view);
+      var results =
+          searcher.search(
+              querySimilarity.scoreFunction,
+              querySimilarity.reRanker,
+              queryParams.numCandidates,
+              Bits.ALL);
 
       return Arrays.stream(results.getNodes())
           .map(nodeScore -> nodeScore.node)
@@ -237,12 +272,128 @@ public class JVectorIndex {
     @Override
     public String description() {
       return String.format(
-          "jvector_vamana_M:%s-beamWidth:%s-neighborOverflow:%s-alpha:%s_numCandidates:%s",
+          "jvector_vamana_M:%s-beamWidth:%s-neighborOverflow:%s-alpha:%s_numCandidates:%s-pqFactor:%s",
           buildParams.M,
           buildParams.beamWidth,
           buildParams.neighborOverflow,
           buildParams.alpha,
-          queryParams.numCandidates);
+          queryParams.numCandidates,
+          queryParams.pqFactor);
+    }
+
+    private record QuerySimilarity(ScoreFunction scoreFunction, ReRanker<float[]> reRanker) {}
+
+    private QuerySimilarity querySimilarity(float[] vector, View<float[]> view) {
+      if (this.compressedVectors.isEmpty()) {
+        return new QuerySimilarity(
+            (ExactScoreFunction) (i -> this.similarityFunction.compare(vector, view.getVector(i))),
+            null);
+      }
+
+      return new QuerySimilarity(
+          compressedVectors.get().approximateScoreFunctionFor(vector, this.similarityFunction),
+          (i, vectors) -> this.similarityFunction.compare(vector, vectors.get(i)));
+    }
+
+    private static RandomAccessVectorValues<float[]> graphViewVectors(
+        View<float[]> view, int dimensions) {
+      return new RandomAccessVectorValues<float[]>() {
+        @Override
+        public int size() {
+          return view.size();
+        }
+
+        @Override
+        public int dimension() {
+          return dimensions;
+        }
+
+        @Override
+        public float[] vectorValue(int i) {
+          return view.getVector(i);
+        }
+
+        @Override
+        public boolean isValueShared() {
+          return false;
+        }
+
+        @Override
+        public RandomAccessVectorValues<float[]> copy() {
+          return this;
+        }
+      };
+    }
+
+    private static Optional<CompressedVectors> compressedVectors(
+        Path indexPath,
+        int dimensions,
+        int pqFactor,
+        GraphIndex<float[]> graph,
+        VectorSimilarityFunction vectorSimilarityFunction)
+        throws IOException {
+      if (pqFactor <= 0) {
+        return Optional.empty();
+      }
+
+      var compressedVectorsFile =
+          indexPath.resolve(String.format(COMPRESSED_VECTOR_FILE_FORMAT, pqFactor));
+      if (compressedVectorsFile.toFile().exists()) {
+        try (var reader = new SimpleMappedReaderSupplier(compressedVectorsFile)) {
+          return Optional.of(CompressedVectors.load(reader.get(), 0));
+        }
+      }
+
+      var pqDims = dimensions / pqFactor;
+
+      LOGGER.info(
+          "index configured with pqFactor of {}, building codebook with {} dimensions",
+          pqFactor,
+          pqDims);
+
+      var graphVectors = graphViewVectors(graph.getView(), dimensions);
+      var size = graph.size();
+
+      List<float[]> vectors;
+      try (var progress = ProgressBar.create("loading vectors from graph", size)) {
+        vectors =
+            IntStream.range(0, size)
+                .sequential()
+                .mapToObj(graphVectors::vectorValue)
+                .peek(ignored -> progress.inc())
+                .collect(Collectors.toList());
+      }
+
+      LOGGER.info("building codebooks");
+      var pqStart = Instant.now();
+      var pq =
+          ProductQuantization.compute(
+              new ListRandomAccessVectorValues(vectors, dimensions),
+              pqDims,
+              vectorSimilarityFunction == VectorSimilarityFunction.EUCLIDEAN);
+      var pqEnd = Instant.now();
+      LOGGER.info("built codebooks in {}", Duration.between(pqStart, pqEnd));
+
+      LOGGER.info("beginning to quantize vectors");
+      var quantizeStart = Instant.now();
+      var quantizedVectors = pq.encodeAll(vectors);
+      var compressedVectors = new CompressedVectors(pq, quantizedVectors);
+      var quantizeEnd = Instant.now();
+      LOGGER.info(
+          "quantized vectors in {}, compressed size: {}",
+          Duration.between(quantizeStart, quantizeEnd),
+          Bytes.ofBytes(compressedVectors.memorySize()));
+
+      LOGGER.info("writing quantized vectors to disk");
+      var writeStart = Instant.now();
+      try (var output =
+          new DataOutputStream(new FileOutputStream(compressedVectorsFile.toFile()))) {
+        compressedVectors.write(output);
+      }
+      var writeEnd = Instant.now();
+      LOGGER.info("wrote quantized vectors in {}", Duration.between(writeStart, writeEnd));
+
+      return Optional.of(compressedVectors);
     }
   }
 }
