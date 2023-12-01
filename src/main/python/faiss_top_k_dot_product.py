5402ebd6ff3c45be7218f20c1bc2223b25c98c62
Kevin Rosendahl <kevin.rosendahl@mongodb.com>
2023-11-30 19:15:43 -0800

    faiss IP

---
+++ src/main/python/faiss_top_k_dot_product.py
@@ -0,0 +1,15 @@
+import numpy as np
+import faiss
+
+dimensions = 768
+
+train = np.fromfile('train.fvecs', dtype='float32').reshape(-1, dimensions)
+test = np.fromfile('test.fvecs', dtype='float32').reshape(-1, dimensions)
+
+index = faiss.IndexFlatIP(dimensions)
+index.add(train)
+
+k = 100
+D, I = index.search(test[0:10000], k)  # `D` is an array of distances, `I` is an array of indices
+I = np.array(I, dtype=np.int32)
+I.tofile('neighbors.ivecs')
